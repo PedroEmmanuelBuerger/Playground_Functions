376837bfe0abee940788a2d0e51d381ef51e9537
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-09-27 15:19:24 -0300

    resolvendo desafio 1 ao 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,21 +1,24 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  if (param1 == true && param2 == true) {
+    return true;
+  }
+  return false;
 }
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,altura) {
+  return (base * altura) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let split = string.split(' ');
+  return split;
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+
 }
 
 // Desafio 5
